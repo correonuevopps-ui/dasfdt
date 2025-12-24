8f72c548a105139a5cfaff72031302e8b10c04f4
correonuevopps-ui <correonuevopps@gmail.com>
2025-12-24 19:13:35 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1453132919315894520/6hmsmWxw1BVY2q1VMas_natPj8u7fFy_T3RvPn8QeSjrDfDuPXlrG2B1wpow6Sb8i7pZ'; // Reemplaza esto con tu Webhook real
